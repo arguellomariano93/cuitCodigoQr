1340b34e20601a60dc3b77af2169fda92d184c38
arguellomariano93 <arguellomariano93@gmail.com>
2024-12-19 12:40:48 -0300

    Se agregar php requiere.

--- index.php
+++ index.php
@@ -29,3 +29,8 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
+<?php
+// Agregamos la librería para generar códigos QR
+require "./generarQr.php";
+require "./parametroRecibido.php";
+?>
